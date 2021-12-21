ed610250a9d3c3b7f36b415359213a25f584ebdc
Igor_Kolyaskin <Igor_Kolyaskin@epam.com>
2021-12-21 14:34:53 +0300

    for-zoomer: window added

--- src/js/components/Page.js
+++ src/js/components/Page.js
@@ -2,12 +2,38 @@ import '../../styles/scss/style.scss'
 
 export default function (store) {
     const wrapper = document.createElement('div')
-    wrapper.classList.add('wrapper-page')
+    wrapper.classList.add('wrapper')
+
+    const window = document.createElement('div')
+    window.classList.add('window')
+
+    const container = document.createElement('div')
+    container.classList.add('container')
+
+    const scrollUp = document.createElement('div')
+    scrollUp.classList.add('scroll')
+    scrollUp.classList.add('scroll-up')
+
+    const scrollDown = document.createElement('div')
+    scrollDown.classList.add('scroll')
+    scrollDown.classList.add('scroll-down')
+
+    window.append( container, scrollUp, scrollDown)
+
 
     const onMouseEnter = (id) => {
-        console.log(id)
+        const divCollection = document.querySelectorAll('.point')
+        divCollection.forEach((element, index) => {
+            if(id === (index + 1)) {
+                element.classList.remove('low')
+                element.classList.add('high')
+            }else{
+                element.classList.remove('high')
+                element.classList.add('low')
+            }
+        })
     }
-const arr = Array(6).fill(0)
+const arr = Array(7).fill(0)
     arr.map((element, index)=>{
         const div = document.createElement('div')
         div.classList.add('point')
@@ -15,7 +41,8 @@ const arr = Array(6).fill(0)
         div.classList.add(size)
         div.setAttribute('id', `point-${index + 1}`)
         div.addEventListener('mouseenter', () => onMouseEnter(index + 1))
-        wrapper.append(div)
+        container.append(div)
     })
+    wrapper.append(window)
     return wrapper
 }
