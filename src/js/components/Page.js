297cb9865a480459ce050d16725e432a2c8141b7
Igor_Kolyaskin <Igor_Kolyaskin@epam.com>
2021-12-21 11:34:54 +0300

    for-zoomer: add listener

--- src/js/components/Page.js
+++ src/js/components/Page.js
@@ -4,36 +4,18 @@ export default function (store) {
     const wrapper = document.createElement('div')
     wrapper.classList.add('wrapper-page')
 
-    const div1 = document.createElement('div')
-    div1.classList.add('point')
-    div1.classList.add('high')
-    div1.setAttribute('id', 'point-1')
-
-    const div2 = document.createElement('div')
-    div2.classList.add('point')
-    div2.classList.add('low')
-    div2.setAttribute('id', 'point-2')
-
-    const div3 = document.createElement('div')
-    div3.classList.add('point')
-    div3.classList.add('low')
-    div3.setAttribute('id', 'point-3')
-
-    const div4 = document.createElement('div')
-    div4.classList.add('point')
-    div4.classList.add('low')
-    div4.setAttribute('id', 'point-4')
-
-    const div5 = document.createElement('div')
-    div5.classList.add('point')
-    div5.classList.add('low')
-    div5.setAttribute('id', 'point-5')
-
-    const div6 = document.createElement('div')
-    div6.classList.add('point')
-    div6.classList.add('low')
-    div6.setAttribute('id', 'point-6')
-
-    wrapper.append(div1, div2, div3, div4, div5, div6)
+    const onMouseEnter = (id) => {
+        console.log(id)
+    }
+const arr = Array(6).fill(0)
+    arr.map((element, index)=>{
+        const div = document.createElement('div')
+        div.classList.add('point')
+        const size = index? 'low': 'high'
+        div.classList.add(size)
+        div.setAttribute('id', `point-${index + 1}`)
+        div.addEventListener('mouseenter', () => onMouseEnter(index + 1))
+        wrapper.append(div)
+    })
     return wrapper
 }
