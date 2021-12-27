9ae3893af4fc418d6cf878d85dfa94565f92da6b
Igor_Kolyaskin <Igor_Kolyaskin@epam.com>
2021-12-27 19:22:37 +0300

    for-zoomer: recursive setTimout works with slow and quick

--- src/js/bll/store.js
+++ src/js/bll/store.js
@@ -2,10 +2,11 @@ export const store = {
     _state: {
         shift: 0,
         windowHeight: 24,
-        elementNumber: 400,
+        elementNumber: 1000,
         shiftDirection: 0,
         scrollRate: 0,
-        delayArray: [0, 400, 40]
+        delayArray: [0, 400, 100],
+        containerHeightArray: [24, 24, 12]
     },
 
     getState() {
@@ -18,36 +19,39 @@ export const store = {
             case 'SHIFT_SLOW_START':
                 this._state.scrollRate = 1
                 this._state.shiftDirection = action.payload.shiftDirection
-                let {elementNumber, windowHeight, delayArray} = this._state
-
+                let {elementNumber, windowHeight, delayArray, containerHeightArray} = this._state
                 let delay = delayArray[1]
+
                 let repeater = setTimeout(function step() {
-                    if(!store._state.scrollRate||
-                        store._state.shift === 0 && store._state.shiftDirection < 0 ||
-                        (store._state.shift === (elementNumber - windowHeight) && store._state.shiftDirection > 0)
-                ) {
+                    const {scrollRate, shift, shiftDirection} = store._state
+
+                    if (!scrollRate ||
+                        shift === 0 && shiftDirection < 0 ||
+                        (shift === (elementNumber - windowHeight) && shiftDirection > 0)
+                    ) {
                         clearTimeout(repeater)
                     } else {
-                        delay = delayArray[store._state.scrollRate]
+                        delay = delayArray[scrollRate]
                         repeater = setTimeout(step, delay);
-                        const newState = store._state.shift + action.payload.shiftDirection
+                        const newState = shift + action.payload.shiftDirection
                         store._state.shift = newState
-                        const listPosition = 48 - newState * 24
+                        const listPosition = 48 - newState * containerHeightArray[scrollRate]
                         container.style.top = `${listPosition}px`
                     }
                 }, delay);
                 break
             case 'SHIFT_SLOW_STOP':
-                this._state.scrollRate= 0
+                this._state.scrollRate = 0
                 break
             case 'SHIFT_QUICK_START':
+                container.style.setProperty('--element-container-height', '12px')
                 this._state.scrollRate = 2
                 break
             case 'SHIFT_QUICK_STOP':
                 this._state.scrollRate = 1
+                container.style.setProperty('--element-container-height', '24px')
                 break
 
-                // container.style.setProperty('--element-container-height', '12px')
 
             default:
                 console.log('default actions')
